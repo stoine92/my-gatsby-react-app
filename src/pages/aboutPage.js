e27a51613a1b03ebf014962550c1d74f4f69ba7c
Stoyan Nikolov <59065882+stoine92@users.noreply.github.com>
2022-08-02 16:48:15 +0100

    Updating my bio

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -2,15 +2,16 @@ import React from "react";
 import { Link } from 'gatsby'
 import Layout from "../components/layout";
 import Head from '../components/head';
+import * as aboutStyle from './aboutPage.module.scss';
 
 
 const AboutPage = () => {
     return(
         <Layout>
             <Head title="About" />
-            <h1> About page title</h1>
-            <p>Here I will have some bio </p>
-            <p><Link to="/contactPage">Contact page</Link></p>
+            <h1> About page</h1>
+            <p className={aboutStyle.paragraph}>Graduated and certified by SoftUni.org, a bootcamp coding academy found by the creators of Telerik. My bootcamp course provided me the skills to learn JavaScript from basics all the way to ReactJs. Thorough our journey I have worked with Vanilla JavaScript, DOM manipulations, HTML, CSS, Gatsby, Sammy, Handlebars, full React course, firebase and much more.</p>
+            <p><Link className={aboutStyle.pLink} to="/contactPage">Contact page</Link></p>
             
         </Layout>
             
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,8 @@ const IndexPage = () => {
     <Layout>
       <Head title="Home" />
           <h1>Hello</h1>
-          <h2> I am Stoyan, a front-end developer. Living in rainy Manchester</h2>
+          <h2> I am Stoyan, a front-end developer. Living in rainy Manchester.</h2>
+          <p> This page is made from scratch powered by ReactJS, GatsbyJS, contentful and Netlify</p>
           <p> Need a developer ? <Link to="/contactPage">Contact me.</Link></p>
     </Layout>
   )
