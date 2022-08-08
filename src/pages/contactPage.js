17ec07293b782c6d9de37a58ff611f318278b4b3
Stoyan Nikolov <59065882+stoine92@users.noreply.github.com>
2022-08-08 15:47:22 +0100

    Finishing touches

--- src/pages/contactPage.js
+++ src/pages/contactPage.js
@@ -9,7 +9,11 @@ const ContactPage = () => {
             <Layout>
                 <Head title="Contact" />
                     <h1> Contact page</h1>
-                    <p>In order to visit my gitHub accoutn please <a href="https://github.com/stoine92">click here</a></p>
+                    <h3>Stoyan Nikolov</h3>
+                    <p>tel: +44 7843 138809</p>
+                    <p><strong>GitHub</strong> link: <a href="https://github.com/stoine92">click here</a></p>
+                    <p><strong>Linkedin</strong> link: <a href="https://www.linkedin.com/in/stoyan-nikolov-660a0491/">click here</a></p>
+                    
             </Layout>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => {
     <Layout>
       <Head title="Home" />
           <h1>Hello</h1>
-          <h2> I am Stoyan, a front-end developer. Living in rainy Manchester.</h2>
-          <p> This page is made from scratch powered by ReactJS, GatsbyJS, contentful and Netlify</p>
+          <h2> I am Stoyan, a front-end developer. Living in lovely Manchester.</h2>
+          <p> This page is made from scratch powered by ReactJS, GatsbyJS, contentful and Netlify <a href="https://github.com/stoine92/my-gatsby-react-app" target="_blank">More info</a></p>
           <p> Need a developer ? <Link to="/contactPage">Contact me.</Link></p>
     </Layout>
   )
